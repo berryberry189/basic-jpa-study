cac2a8ebeee05227fb0f9e1f4fde96e5a3d86c7b
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2021-06-28 21:38:19 +0900

    영속성 컨텍스트 기본 설명

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -30,7 +30,22 @@ public class JpaMain {
             System.out.println("findMember = " + findMember.getName());*/
 
             // 목록
-            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+            //List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+
+            // == 영속성 컨텍스트 설명 == //
+            // 비영속 상태 (new)
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+
+            // 영속 상태 => DB저장 상태 X (쿼리가 날라가는 시점 : 아래의 tx.commit();)
+            em.persist(member);
+
+            // 준영속 => 회원 엔티티를 영속성 컨텍스트에서 분리
+            em.detach(member);
+
+            // 객체를 삭제
+            em.remove(member);
 
             tx.commit();
         } catch (Exception e){
