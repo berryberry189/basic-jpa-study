669b4db76708bf523879b736549432b6cb8f174f
berryberry189 <58412521+berryberry189@users.noreply.github.com>
2021-06-29 23:49:15 +0900

    기본 키 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,41 +18,14 @@ public class JpaMain {
         tx.begin();
 
         try{
-            // 등록
-           /* Member member = new Member();
+
+            Member member = new Member();
             member.setId(1L);
-            member.setName("HelloA");
-            em.persist(member);*/
+            member.setUsername("A");
+            member.setRoleType(RoleType.USER);
 
-            // 수정
-            /*Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJpa"); // em.persist(member); 안해도 됨
-            System.out.println("findMember = " + findMember.getName());*/
+            em.persist(member);
 
-            // 목록
-            //List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
-
-            // == 영속성 컨텍스트 설명 == //
-            // 비영속 상태 (new)
-            Member member1 = new Member(101L, "A");
-            Member member2 = new Member(102L, "B");
-
-
-            // 영속 상태 => DB저장 상태 X (쿼리가 날라가는 시점 : 아래의 tx.commit();), 모아서 한번에 보냄
-            em.persist(member1);
-            em.persist(member2);
-
-            // 준영속 => 회원 엔티티를 영속성 컨텍스트에서 분리
-            // em.detach(member); // 특정 엔티티만 준영속 상태로 전환
-            // em.clear(); // 영속성 컨텍스트를 완전히 초기화
-            // em.close(); // 영속성 컨텍스트를 종료
-
-
-            // 객체를 삭제 DB에 imsert query를 보낸다.
-            //em.remove(member);
-
-
-            // 커밋하는 순간
             tx.commit();
         } catch (Exception e){
             tx.rollback();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,24 +1,51 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-// @Table(name="MER") => 매핑시 테이블 명 지정
 public class Member {
 
     @Id
     private Long id;
 
-    // @Column(unique = true, length = 10) DB에 영향을 주는 옵션
-    private String name;
+    @Column(name="name", insertable = true, updatable = false) // insert에는 반영되지만 update시에는 반영 안되도록 설정
+    private String username;
+
+    //@Column(nullable = false) // null 허용 여부
+    @Column(columnDefinition = "integer(2) default 20") // DB에 컬럼정보를 직접 줄 때 사용
+    private Integer age;
+    
+    @Enumerated(EnumType.STRING) // ORDINAL 쓰지말자!! (index 숫자로 저장되기 때문..)
+    private RoleType roleType;
+    
+    @Temporal(TemporalType.TIMESTAMP) // 닐짜 타입
+    private Date createdDate;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    
+    @Lob // 큰 컨텐츠, 지정 할 수 있는 속성이 없음
+    private String description;
+
+    @Transient // 특정 필드를 컬럼에 매핑하지 않음(매핑 무시, DB에는 생성X )
+    private int temp;
+
+    /**
+     create table Member (
+         id bigint not null,
+         age integer,
+         createdDate timestamp,
+         description clob,
+         lastModifiedDate timestamp,
+         roleType varchar(255),
+         name varchar(255),
+         primary key (id)
+     )
+     */
+
 
     public Member() {
-    }
-
-    public Member(long l, String a) {
     }
 
     public Long getId() {
@@ -29,11 +56,59 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
